9d0542e54c72eddf0f70d3d892ebc2f09596eabb
Ryan Priebe <rpriebe@me.com>
2018-08-03 15:51:11 -0600

    updated helper::rolling_log so it can be used to write generic or time-based messages to the rolling log

--- src/helper/rolling_log.rs
+++ src/helper/rolling_log.rs
@@ -4,7 +4,23 @@ use std::fs::OpenOptions;
 use std::io::prelude::*;
 use std::env;
 
-pub fn update(message: String) {
+fn write_generic(message: String) {
+    match env::home_dir() {
+        Some(path) => {
+            let mut file = OpenOptions::new()
+                .append(true)
+                .open(format!("{}/.evertils/rolling.log", path.display()))
+                .unwrap();
+
+            if let Err(e) = write!(file, "{}", message) {
+                println!("Error writing to rolling log: {}", e);
+            }
+        },
+        None => println!("Impossible to get your home dir!"),
+    }
+}
+
+fn write(message: String) {
     match env::home_dir() {
         Some(path) => {
             let mut file = OpenOptions::new()
@@ -21,4 +37,15 @@ pub fn update(message: String) {
         },
         None => println!("Impossible to get your home dir!"),
     }
+}
+
+pub fn update(message: String) {
+    self::write(message);
+}
+
+pub fn new_day() {
+    let date = Local::now();
+    let message: String = format!("=====================\n{}\n=====================", date);
+
+    self::write_generic(message);
 }
