ede8fac7babdb6897adf060de17d43941cfb4dc1
Ryan Priebe <rpriebe@me.com>
2018-08-03 16:01:19 -0600

    implemented logm command

--- src/logm.rs
+++ src/logm.rs
@@ -1,2 +1,33 @@
 // log monitoring items
-fn main() {}
+extern crate chrono;
+
+mod helper;
+
+mod log {
+    use std::env;
+    use std::process::Command;
+    use helper;
+
+    pub fn exec() {
+        let args: Vec<String> = env::args().collect();
+        let message = &args[1];
+
+        let output = Command::new("sh")
+                .arg("-c")
+                .arg(format!("evertils log message \"monitoring - {}\"", message))
+                .output()
+                .expect("failed to execute");
+
+        // TODO: this prints on 2 lines, should only print on one
+        // let rlog_msg: String = format!("{} - {}", job_number, message);
+        let rlog_msg: String = message.to_owned();
+        // make sure the data is appended to the rolling log
+        helper::rolling_log::update(rlog_msg);
+
+        let _ = output.stdout;
+    }
+}
+
+fn main() {
+    log::exec();
+}
